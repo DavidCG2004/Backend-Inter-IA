58a0f7cdeb70a0f27095e90941ed199be11f1228
DavidCG2004 <Alexandergarcia215@outlook.com>
2025-11-30 21:08:58 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv'
-import app from './server.js'
-import connection from './database.js'
-
-dotenv.config()     // ✅ PRIMERO cargas variables
-connection()       // ✅ LUEGO conectas a MongoDB
-
-export default app
