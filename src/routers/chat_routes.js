2231748baa007d6487f377d5f8826571de9d1d08
DavidCG2004 <Alexandergarcia215@outlook.com>
2026-02-02 05:35:31 -0500

    Delete src directory

--- src/routers/chat_routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { getChatHistory } from '../controllers/chat_controller.js'
-import { verificarTokenJWT } from '../middlewares/JWT.js'
-
-const router = Router()
-
-router.get('/history', verificarTokenJWT, getChatHistory)
-
-export default router
