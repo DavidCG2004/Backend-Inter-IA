a83533ee760ef872bc9583f8edcabf55d88cc2a3
DavidCG2004 <Alexandergarcia215@outlook.com>
2026-02-03 12:13:08 -0500

    Refactor passport.js to export configurePassport function
    Refactor passport configuration to export a function for setup.

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,36 +2,38 @@ import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import Usuario from '../models/Usuario.js';
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.URL_BACKEND}/auth/google/callback`
-  },
-  async (accessToken, refreshToken, profile, done) => {
+export const configurePassport = () => {
+    if (passport._strategy('google')) return passport;
+
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: `${process.env.URL_BACKEND}auth/google/callback`
+    },
+    async (accessToken, refreshToken, profile, done) => {
     try {
-        let usuario = await Usuario.findOne({ googleId: profile.id });
-        if (!usuario) {
-            usuario = await Usuario.findOne({ email: profile.emails[0].value });
-            if (usuario) {
-                usuario.googleId = profile.id;
-                await usuario.save();
-            } else {
-                usuario = new Usuario({
-                    nombre: profile.name.givenName,
-                    apellido: profile.name.familyName || "",
-                    email: profile.emails[0].value,
-                    googleId: profile.id,
-                    confirmEmail: true
-                });
-                await usuario.save();
+            let usuario = await Usuario.findOne({ googleId: profile.id });
+            if (!usuario) {
+                usuario = await Usuario.findOne({ email: profile.emails[0].value });
+                if (usuario) {
+                    usuario.googleId = profile.id;
+                    await usuario.save();
+                } else {
+                    usuario = new Usuario({
+                        nombre: profile.name.givenName,
+                        apellido: profile.name.familyName || "",
+                        email: profile.emails[0].value,
+                        googleId: profile.id,
+                        confirmEmail: true
+                    });
+                    await usuario.save();
+                }
             }
+            return done(null, usuario);
+        } catch (error) {
+            return done(error, null);
         }
-        return done(null, usuario);
-    } catch (error) {
-        return done(error, null);
-    }
-  }
-
+      }
 ));
-
-
+    return passport;
+};
